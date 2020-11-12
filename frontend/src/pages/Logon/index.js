3900ee8903c133895f5eca5c3914c2c623061553
julioc98 <julioc.santos98@gmail.com>
2020-11-11 21:03:39 -0300

    first commit

---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiLogIn } from 'react-icons/fi'
+
+import './styles.css';
+
+import logoImg from '../../assets/logo.svg';
+import heroesImg from '../../assets/heroes.png';
+import api from '../../services/api';
+
+export default function Logon() {
+    const [id, setId] = useState('');
+    const history = useHistory();
+
+    async function handleLogin(e) {
+        e.preventDefault();
+
+        try {
+            const response = await api.post('sessions', { id });
+
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', response.data.name);
+
+            history.push('/profile');
+        } catch (err) {
+            alert('Falha no login, tente novamente.');
+        }
+    }
+
+    return (
+        <div className='back'>
+            <div className='cover'>
+                <img src={heroesImg} alt="Heroes" />
+            </div>
+            <div className="logon-container">
+                <section className="form">
+                    <img className="logo" src={logoImg} alt="Be The Hero" />
+
+                    <form onSubmit={handleLogin}>
+                        <input
+                            placeholder="E-Mail"
+                            value={id}
+                            onChange={e => setId(e.target.value)}
+                        />
+                        <input
+                            placeholder="Senha"
+                            value={id}
+                            onChange={e => setId(e.target.value)}
+                        />
+                        <button className="button" type="submit">Entrar</button>
+
+                        <Link className="back-link" to="/register">
+                            <FiLogIn size={16} color="#ff6682" />
+                            Não tenho cadastro
+                        </Link>
+                    </form>
+                </section>
+            </div>
+        </div>
+    );
+}
---
+++ frontend/src/pages/Register/index.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi'
+
+import api from '../../services/api';
+import './styles.css';
+
+import logoImg from '../../assets/logo.svg';
+import coverRegister from '../../assets/cover-register.png';
+
+export default function Register() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [whatsapp, setWhatsapp] = useState('');
+    const [city, setCity] = useState('');
+    const [uf, setUf] = useState('');
+
+    const history = useHistory();
+
+    async function handleRegister(e) {
+        e.preventDefault();
+
+        const data = {
+            name,
+            email,
+            whatsapp,
+            city,
+            uf,
+        };
+
+        try {
+            const response = await api.post('ongs', data);
+            alert(`Seu ID de acesso: ${response.data.id}`);
+            history.push('/');
+        } catch (error) {
+            alert('Erro no cadastro, tente novamente.');
+        }
+    }
+
+    return (
+        <div className="register-container">
+            <div className="content">
+                <section>
+                    <img src={logoImg} alt="Be The Hero" />
+
+                    <h1>Cadastro</h1>
+                    <p>Faça seu cadastro, entre na plataforma e planeje sua vida com muito mais facilidade.</p>
+                    <form onSubmit={handleRegister}>
+                        <input
+                            placeholder="Nome Completo"
+                            value={name}
+                            onChange={e => setName(e.target.value)}
+                        />
+
+                        <input
+                            type="email"
+                            placeholder="E-Mail"
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                        />
+
+                        <input
+                            placeholder="WhatsApp"
+                            value={whatsapp}
+                            onChange={e => setWhatsapp(e.target.value)}
+                        />
+                        <input
+                            placeholder="Senha"
+                            value={whatsapp}
+                            onChange={e => setWhatsapp(e.target.value)}
+                        />
+                        <input
+                            placeholder="Confirmar Senha"
+                            value={whatsapp}
+                            onChange={e => setWhatsapp(e.target.value)}
+                        />
+                        <button className="button" type="submit">Cadastrar</button>
+                        <Link className="back-link" to="/">
+                            <FiArrowLeft size={16} color="#ff6583" />
+                        Já tenho cadastro
+                    </Link>
+                    </form>
+                </section>
+            </div>
+
+            <div className="register-cover">
+                <img src={coverRegister} alt="Canvas Projeto de Vida" />
+            </div>
+        </div>
+    );
+}
